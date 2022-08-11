a4305a7836903fd55b3cefcf842c106d14d589db
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-11 13:25:38 -0400

    Got very basic AI working.
    Got very basic AI working.

--- script.js
+++ script.js
@@ -1,13 +1,78 @@
 let playerTurn = true;
 
-function placeMarker() {
+const player = (playerName, playerIcon) => {
 
-    if (playerTurn)
-        this.style.backgroundImage = "url('o.svg')";
-    else
-        this.style.backgroundImage = "url('x.svg')"
+    let name = playerName;
+    let icon = playerIcon;
+    let score = 0;
+
+    let moves = [
+        false, false, false,
+        false, false, false,
+        false, false, false
+    ];
+
+    return { name, icon, score, moves };
+}
+
+const player1 = player("Wake", "x");
+const AI = player("AI", "o");
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function AITurn() {
+
+    AI.moves = player1.moves;
+
+    let indexList = [];
+    for (let i = 0; i < AI.moves.length; i++) {
+        if (AI.moves[i] === false)
+            indexList.push(i);
+    }
+
+    console.table(indexList);
+
+    let squareID = parseInt(indexList[getRandomInt(indexList.length)]);
+    const square = document.getElementById(squareID);
+
+    if (AI.icon === "x")
+        square.style.backgroundImage = "url('x.svg')";
+    else if (AI.icon === "o")
+        square.style.backgroundImage = "url('o.svg')";
+
+    AI.moves[squareID] = true;
 
     playerTurn = !playerTurn;
+
+}
+
+
+
+function placeMarker() {
+
+    player1.moves = AI.moves;
+
+    if (playerTurn) {
+
+        if (player1.moves[this.id] === false) {
+
+            if (player1.icon === "x")
+                this.style.backgroundImage = "url('x.svg')"
+            else
+                this.style.backgroundImage = "url('o.svg')";
+
+            player1.moves[this.id] = true;
+
+            playerTurn = !playerTurn;
+
+            AITurn();
+        }
+        else {
+            alert("Can't Move There!");
+        }
+    }
 }
 
 function drawGrid() {
