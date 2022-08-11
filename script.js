721ad3c3a88c229f7b42750fb302b6ede72944ea
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-11 15:46:28 -0400

    Made AI smarter by responding to Player about to win.
    Created AI function on Player object.
    
    Made AI  smarter by responding to Player about to win.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let playerTurn = true;
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
 const player = (playerName, playerIcon) => {
 
@@ -11,39 +14,437 @@ const player = (playerName, playerIcon) => {
         "--", "--", "--"
     ];
 
-    return { name, icon, score, moves };
-}
+    const analyzeMoves = () => {
 
+        let blank = [];
+        let opponent = []
+        let mine = []
+        for (let i = 0; i < moves.length; i++) {
+            if (icon === "x") {
+                if (moves[i] === "x")
+                    mine.push(i);
+                else if (moves[i] === "o")
+                    opponent.push(i);
+                else if (moves[i] === "--")
+                    blank.push(i);
+            }
+            else {
+
+                if (moves[i] === "o")
+                    mine.push(i);
+                else if (moves[i] === "x")
+                    opponent.push(i);
+                else if (moves[i] === "--")
+                    blank.push(i);
+            }
+        }
+
+        if (opponent.length === 1) {
+
+            let squareID = parseInt(blank[getRandomInt(blank.length)]);
+            const square = document.getElementById(squareID);
+
+            if (AI.icon === "x") {
+                square.style.backgroundImage = "url('x.svg')";
+                AI.moves[squareID] = "x";
+            }
+            else {
+                square.style.backgroundImage = "url('o.svg')";
+                AI.moves[squareID] = "o";
+            }
+        }
+        else {
+            // Rows
+            // 0 1  -> is 2 blank ? place : don't
+            // 0 2   -> is 1 blank ? place : don't
+            // 1 2  -> is 0 blank ? place : don't
+            if (opponent.includes(0) && opponent.includes(1)) {
+
+                if (blank.includes(2)) {
+                    const square = document.getElementById(2);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[2] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[2] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(0) && opponent.includes(2)) {
+                if (blank.includes(1)) {
+                    const square = document.getElementById(1);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[1] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[1] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(1) && opponent.includes(2)) {
+                if (blank.includes(0)) {
+                    const square = document.getElementById(0);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[0] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[0] = "o";
+                    }
+                }
+            }
+
+
+            // 3 4  -> is 5 blank ? place : don't
+            // 3 5  -> is 4 blank ? place : don't
+            // 4 5  -> is 3 blank ? place : don't
+            if (opponent.includes(3) && opponent.includes(4)) {
+
+                if (blank.includes(5)) {
+                    const square = document.getElementById(5);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[5] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[5] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(3) && opponent.includes(5)) {
+                if (blank.includes(4)) {
+                    const square = document.getElementById(4);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[4] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[4] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(4) && opponent.includes(5)) {
+                if (blank.includes(3)) {
+                    const square = document.getElementById(3);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[3] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[3] = "o";
+                    }
+                }
+            }
+
+
+            // 6 7  -> is 8  blank ? place : don't
+            // 6 8   -> is 7 blank ? place : don't
+            // 7 8  -> is 6 blank ? place : don't
+            else if (opponent.includes(6) && opponent.includes(7)) {
+
+                if (blank.includes(8)) {
+                    const square = document.getElementById(8);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[8] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[8] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(6) && opponent.includes(8)) {
+                if (blank.includes(7)) {
+                    const square = document.getElementById(7);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[7] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[7] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(7) && opponent.includes(8)) {
+                if (blank.includes(6)) {
+                    const square = document.getElementById(6);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[6] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[6] = "o";
+                    }
+                }
+            }
+
+            // Columns
+            // 0 3   -> is  6 blank ? place : don't
+            // 0 6   -> is 3 blank ? place : don't
+            // 3 6  -> is 0 blank ? place : don't
+            else if (opponent.includes(0) && opponent.includes(3)) {
+
+                if (blank.includes(6)) {
+                    const square = document.getElementById(6);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[6] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[6] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(0) && opponent.includes(6)) {
+                if (blank.includes(3)) {
+                    const square = document.getElementById(3);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[3] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[3] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(3) && opponent.includes(6)) {
+                if (blank.includes(0)) {
+                    const square = document.getElementById(0);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[0] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[0] = "o";
+                    }
+                }
+            }
+
+            // 1 4  -> is 7 blank ? place : don't
+            // 1 7  -> is 4 blank ? place : don't
+            // 4 7  -> is 1 blank ? place : don't
+            else if (opponent.includes(1) && opponent.includes(4)) {
+
+                if (blank.includes(7)) {
+                    const square = document.getElementById(7);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[7] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[7] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(1) && opponent.includes(7)) {
+                if (blank.includes(4)) {
+                    const square = document.getElementById(4);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[4] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[4] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(4) && opponent.includes(7)) {
+                if (blank.includes(1)) {
+                    const square = document.getElementById(1);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[1] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[1] = "o";
+                    }
+                }
+            }
+
+            // 2 5 -> is 8 blank ? place : don't
+            // 2 8 -> is 5 blank ? place : don't
+            // 5 8 -> is 2 blank ? place : don't
+            else if (opponent.includes(2) && opponent.includes(5)) {
+
+                if (blank.includes(8)) {
+                    const square = document.getElementById(8);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[8] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[8] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(2) && opponent.includes(8)) {
+                if (blank.includes(5)) {
+                    const square = document.getElementById(5);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[5] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[5] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(5) && opponent.includes(8)) {
+                if (blank.includes(2)) {
+                    const square = document.getElementById(2);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[2] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[2] = "o";
+                    }
+                }
+            }
+
+            // Diagonal
+            // 0 4  -> is 8 blank ? place : don't
+            // 0 8 -> is 4 blank ? place : don't
+            // 4 8 -> is 0 blank ? place : don't
+            else if (opponent.includes(0) && opponent.includes(4)) {
+
+                if (blank.includes(8)) {
+                    const square = document.getElementById(8);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[8] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[8] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(0) && opponent.includes(8)) {
+                if (blank.includes(4)) {
+                    const square = document.getElementById(5);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[4] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[4] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(4) && opponent.includes(8)) {
+                if (blank.includes(0)) {
+                    const square = document.getElementById(0);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[0] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[0] = "o";
+                    }
+                }
+            }
+
+            // 2 4 -> is 6 blank ? place : don't
+            // 2 6  -> is 4 blank ? place : don't
+            // 4 6 -> is 2 blank ? place : don't
+            else if (opponent.includes(2) && opponent.includes(4)) {
+
+                if (blank.includes(6)) {
+                    const square = document.getElementById(6);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[6] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[6] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(2) && opponent.includes(6)) {
+                if (blank.includes(4)) {
+                    const square = document.getElementById(5);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[4] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[4] = "o";
+                    }
+                }
+            }
+            else if (opponent.includes(4) && opponent.includes(6)) {
+                if (blank.includes(2)) {
+                    const square = document.getElementById(2);
+                    if (AI.icon === "x") {
+                        square.style.backgroundImage = "url('x.svg')";
+                        AI.moves[2] = "x";
+                    }
+                    else {
+                        square.style.backgroundImage = "url('o.svg')";
+                        AI.moves[2] = "o";
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    return { name, icon, score, moves, analyzeMoves };
+}
 const player1 = player("Wake", "x");
 const AI = player("AI", "o");
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
 
 function AITurn() {
 
     AI.moves = player1.moves;
 
-    let indexList = [];
-    for (let i = 0; i < AI.moves.length; i++) {
-        if (AI.moves[i] === "--")
-            indexList.push(i);
-    }
+    // let indexList = [];
+    // for (let i = 0; i < AI.moves.length; i++) {
+    //     if (AI.moves[i] === "--")
+    //         indexList.push(i);
+    // }
 
-    console.table(indexList);
+    // console.table(indexList);
 
-    let squareID = parseInt(indexList[getRandomInt(indexList.length)]);
-    const square = document.getElementById(squareID);
+    // let squareID = parseInt(indexList[getRandomInt(indexList.length)]);
+    // const square = document.getElementById(squareID);
 
-    if (AI.icon === "x") {
-        square.style.backgroundImage = "url('x.svg')";
-        AI.moves[squareID] = "x";
-    }
-    else {
-        square.style.backgroundImage = "url('o.svg')";
-        AI.moves[squareID] = "o";
-    }
+    // if (AI.icon === "x") {
+    //     square.style.backgroundImage = "url('x.svg')";
+    //     AI.moves[squareID] = "x";
+    // }
+    // else {
+    //     square.style.backgroundImage = "url('o.svg')";
+    //     AI.moves[squareID] = "o";
+    // }
+
+    AI.analyzeMoves();
     playerTurn = !playerTurn;
 }
 
